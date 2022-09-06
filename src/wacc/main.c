deb106baa9470c6f77cb5e7294079ed8209b3247
Kyle Coffey <me@phytolizer.dev>
2022-09-05 23:13:05 -0500

    no yaep (2)

--- src/wacc/main.c
+++ src/wacc/main.c
@@ -1,16 +1,7 @@
-#include "embedded/grammar.yaep.h"
-
 #include <stdbool.h>
 #include <stdio.h>
-#include <yaep.h>
 
 int main()
 {
-    struct grammar* g = yaep_create_grammar();
-    if (yaep_parse_grammar(g, true, GRAMMAR_YAEP) != 0)
-    {
-        (void)fprintf(stderr, "Failed to parse grammar\n");
-        return 1;
-    }
     printf("Hello, World!\n");
 }
