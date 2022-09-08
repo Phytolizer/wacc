7178c5329ee16c7db80581e7957fd3a8849fe590
Kyle Coffey <me@phytolizer.dev>
2022-09-07 23:26:26 -0500

    delet 2

--- src/wacc/main.c
+++ src/wacc/main.c
@@ -1,25 +1,9 @@
 #include "wacc/ast.h"
 
-#include <dparse.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-extern D_ParserTables parser_tables_wacc;
-
 int main()
 {
-    D_Parser* p = new_D_Parser(&parser_tables_wacc, sizeof(WaccNode));
-    p->save_parse_tree = true;
-    str test_input = str_lit("int main() { return 0; }");
-    D_ParseNode* pn = dparse(p, (char*)test_input.ptr, (int)test_input.len);
-    if (pn != NULL)
-    {
-        printf("Parsed successfully\n");
-        free_D_ParseNode(p, pn);
-    }
-    else
-    {
-        printf("Parse failed\n");
-    }
     printf("Hello, World!\n");
 }
